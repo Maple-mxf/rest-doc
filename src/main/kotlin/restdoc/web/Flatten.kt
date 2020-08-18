cfa4ce544c9c3f2e5435b4f0452e1eef2d7df4c3
quanminbaoxiao <quanminbaoxiao@163.com>
2020-08-18 19:25:07 +0800

    add dokka plugin

--- src/main/kotlin/restdoc/web/Flatten.kt
+++ src/main/kotlin/restdoc/web/Flatten.kt
@@ -2,7 +2,7 @@ package restdoc.web
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import jdk.nashorn.internal.ir.ObjectNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import org.springframework.stereotype.Component
 import restdoc.model.BodyFieldDescriptor
 import restdoc.model.JSONFieldNode
