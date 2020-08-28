a114fe9a26dd80a1c7267de7a7aaf2dedb96946d
quanminbaoxiao <quanminbaoxiao@163.com>
2020-08-28 15:23:38 +0800

    修正项目编译配置

---
+++ src/main/resources/static/admin/js/doc/header.js
@@ -0,0 +1,46 @@
+var oneheaderline = "\n" +
+    "            <div class=\"layui-form-item one-request-header-line\" id=\"first-header-field\">\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "<!--                    <label class=\"layui-form-label\">请求头</label>-->\n" +
+    "                    <div class=\"layui-input-inline\">\n" +
+    "                        <input type=\"text\" name=\"headerKey\" placeholder=\"Key\" autocomplete=\"off\"\n" +
+    "                               value=\"Content-Type\" class=\"layui-input\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <label>=</label>\n" +
+    "                </div>\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "<!--                    <label class=\"layui-form-label\">值</label>-->\n" +
+    "                    <div class=\"layui-input-inline\">\n" +
+    "                        <input type=\"text\" name=\"headerValue\" placeholder=\"Value\" autocomplete=\"off\"\n" +
+    "                               value=\"application/json;utf-8\"\n" +
+    "                               class=\"layui-input\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <label class=\"layui-form-label\">说明</label>\n" +
+    "                    <div class=\"layui-input-inline\">\n" +
+    "                        <input type=\"text\" placeholder=\"请求内容格式说明\" name=\"headerDescription\" autocomplete=\"off\"\n" +
+    "                               class=\"layui-input\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <div class=\"layui-input-inline\" style=\"width: 5%\">\n" +
+    "                        <input type=\"checkbox\" checked name=\"headerConstraint\" title=\"必选\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <button type=\"button\" class=\"layui-btn layui-btn-sm layui-btn-primary\" id=\"deleteHeaderBtn\"\n" +
+    "                            style=\"margin-left: 10px\"\n" +
+    "                            onclick=\"deleteHeaderFieldLine(this)\">\n" +
+    "                        <i class=\"layui-icon layui-icon-subtraction\" style=\"font-size: 30px; color: black;\"></i>\n" +
+    "                    </button>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "\n" +
+    "        ";
---
+++ src/main/resources/static/admin/js/doc/urivar.js
@@ -0,0 +1,35 @@
+var oneuriline = "            <div class=\"layui-form-item one-request-header-line\" id=\"first-pathvar-field\">\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <div class=\"layui-input-inline\">\n" +
+    "                        <input type=\"text\" name=\"uriKey\" placeholder=\"Key\" autocomplete=\"off\"\n" +
+    "                               class=\"layui-input\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <label>=</label>\n" +
+    "                </div>\n" +
+    "\n" +
+    "                <div class=\"layui-inline\">\n" +
+    "                    <div class=\"layui-input-inline\">\n" +
+    "                        <input type=\"text\" name=\"uriVal\" placeholder=\"Value\" autocomplete=\"off\"\n" +
+    "                               class=\"layui-input\">\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "\n" +
+    "<textarea name='uriDesc' style='display: none' cols='30' rows='10'></textarea>                   " +
+
+    "\n" +
+    " <div class=\"layui-inline\">\n" +
+    "                    <button type=\"button\" title='点击显示字段备注' onclick='addUriVarDescBtn(this)' class=\"layui-btn layui-btn-sm layui-btn-primary" +
+    "                            style=\"margin-left: 10px\"\n" +
+    "                           >\n" +
+    "                        <i class=\"layui-icon layui-icon-tips\" style=\"font-size: 30px; color: black;\"></i>\n" +
+    "                    </button>\n" +
+    "                    <button title='删除当前字段' type=\"button\" class=\"layui-btn layui-btn-sm layui-btn-primary\" id=\"deletePathvarBtn\"\n" +
+    "                            style=\"margin-left: 10px\"\n" +
+    "                            onclick=\"deleteUriVarFieldLine(this)\">\n" +
+    "                        <i class=\"layui-icon layui-icon-subtraction\" style=\"font-size: 30px; color: black;\"></i>\n" +
+    "                    </button>\n" +
+    "                </div>\n" +
+    "            </div>";
