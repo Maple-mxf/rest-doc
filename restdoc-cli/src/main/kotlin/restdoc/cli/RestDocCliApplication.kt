864b8ef1ef3844acd6a017ba7983537254e2eb1c
quanminbaoxiao <quanminbaoxiao@163.com>
2020-09-01 18:33:02 +0800

    删除模块

--- restdoc-cli/src/main/kotlin/restdoc/cli/RestDocCliApplication.kt
+++
@@ -1,9 +0,0 @@
-package restdoc.cli
-
-import org.springframework.boot.runApplication
-
-open class RestDocCliApplication
-
-fun main(args: Array<String>) {
-    runApplication<RestDocCliApplication>(*args)
-}
