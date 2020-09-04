fbd89f5850510374dec7833980a137bb9aad5ceb
quanminbaoxiao <quanminbaoxiao@163.com>
2020-09-04 18:43:16 +0800

    完善rpc调用

---
+++ restdoc-client/src/main/java/restdoc/client/EndpointsListener.java
@@ -0,0 +1,28 @@
+package restdoc.client;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Map;
+
+/**
+ * 导入客户端所有的API 并且生成文档
+ */
+@Component
+public class EndpointsListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = event.getApplicationContext()
+                .getBean(RequestMappingHandlerMapping.class)
+                .getHandlerMethods();
+
+        handlerMethods.forEach((info, method) -> {
+            System.err.println(info.getPatternsCondition().getPatterns());
+        });
+    }
+}
--- restdoc-client/src/main/java/restdoc/client/RestDocClientApplication.java
+++ restdoc-client/src/main/java/restdoc/client/RestDocClientApplication.java
@@ -1,12 +1,18 @@
 package restdoc.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RestDocClientApplication {
 
+    @Autowired
+    ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(RestDocClientApplication.class);
+
     }
 }
