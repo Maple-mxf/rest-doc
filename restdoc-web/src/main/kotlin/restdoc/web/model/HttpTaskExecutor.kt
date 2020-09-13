100b1bd1aaef715468a9db829e29253d20d48647
maxuefeng <m17793873123@163.com>
2020-09-13 11:10:49 +0800

    解决jackson序列化BUG

--- restdoc-web/src/main/kotlin/restdoc/web/model/HttpTaskExecutor.kt
+++ restdoc-web/src/main/kotlin/restdoc/web/model/HttpTaskExecutor.kt
@@ -2,6 +2,7 @@ package restdoc.web.model
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
@@ -14,7 +15,15 @@ class HttpTaskExecutor {
     private lateinit var restTemplate: RestTemplate
 
     fun execute(capture: HttpCommunicationCaptureBody): ResponseEntity<Any?>? {
-        val httpEntity = HttpEntity(capture.requestBody, capture.requestHeaders)
+
+        val requestHeaders = HttpHeaders()
+
+        capture.requestHeaders.forEach { (k, v) ->
+            requestHeaders.addAll(k, v)
+        }
+
+
+        val httpEntity = HttpEntity(capture.requestBody, requestHeaders)
         return restTemplate.exchange(capture.url, capture.method, httpEntity, Any::class.java, capture.uriVariables)
     }
 }
