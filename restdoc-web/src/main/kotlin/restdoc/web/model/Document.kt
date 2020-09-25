a23d6ad07de6ae1c522f86994b3026c4d7f871e9
quanminbaoxiao <quanminbaoxiao@163.com>
2020-09-25 16:39:46 +0800

    增加工具

--- restdoc-web/src/main/kotlin/restdoc/web/model/Document.kt
+++ restdoc-web/src/main/kotlin/restdoc/web/model/Document.kt
@@ -41,6 +41,7 @@ class MethodParamDescriptor {
      *
      */
     var description: String? = ""
+
 }
 
 
---
+++ restdoc-web/src/main/kotlin/restdoc/web/util/ThrowableAction.java
@@ -0,0 +1,35 @@
+package restdoc.web.util;
+
+import java.util.function.Function;
+
+/**
+ * A function interface for action with {@link Throwable}
+ *
+ * @see Function
+ * @see Throwable
+ * @since 2.7.5
+ */
+@FunctionalInterface
+public interface ThrowableAction {
+
+    /**
+     * Executes the action
+     *
+     * @throws Throwable if met with error
+     */
+    void execute() throws Throwable;
+
+    /**
+     * Executes {@link ThrowableAction}
+     *
+     * @param action {@link ThrowableAction}
+     * @throws RuntimeException wrap {@link Exception} to {@link RuntimeException}
+     */
+    static void execute(ThrowableAction action) throws RuntimeException {
+        try {
+            action.execute();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
