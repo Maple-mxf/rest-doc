64a9cbfde3c32e540c35931af82589ba5b66ae2f
maxuefeng <m17793873123@163.com>
2020-11-10 10:04:03 +0800

    优化控制台显示页面

--- restdoc-web/src/main/kotlin/restdoc/web/controller/DocumentNavViewController.kt
+++ restdoc-web/src/main/kotlin/restdoc/web/controller/DocumentNavViewController.kt
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import restdoc.remoting.common.ApplicationType
 import restdoc.web.base.auth.Verify
+import restdoc.web.core.schedule.ClientRegistryCenter
 import restdoc.web.model.ProjectType
 import restdoc.web.repository.ProjectRepository
 
@@ -16,11 +18,13 @@ class DocumentNavViewController {
     @Autowired
     private lateinit var projectRepository: ProjectRepository
 
+    @Autowired
+    private lateinit var clientRegistryCenter: ClientRegistryCenter
+
     @GetMapping("/{projectId}/document/nav/view")
     fun index(@PathVariable projectId: String, model: Model): String {
 
         model.addAttribute("projectId", projectId)
-
 
         val project = projectRepository.findById(projectId)
                 .orElseThrow(restdoc.web.core.Status.BAD_REQUEST::instanceError)
@@ -29,6 +33,8 @@ class DocumentNavViewController {
 
         return when {
             ProjectType.REST_WEB == project.type -> {
+                val instanceNumber = clientRegistryCenter.getClientKeysFilterApplicationType(ApplicationType.REST_WEB).size
+                model.addAttribute("instanceNumber", instanceNumber)
                 "explorer/nav"
             }
             ProjectType.DUBBO.equals(project.type) -> {
