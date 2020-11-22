7db013442bdd3cfbd112a2253e44539db40fa94f
maxuefeng <m17793873123@163.com>
2020-11-22 21:00:43 +0800

    增加POI文档导出依赖

---
+++ restdoc-web/src/main/kotlin/restdoc/web/service/RestWebDocumentService.kt
@@ -0,0 +1,23 @@
+package restdoc.web.service
+
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment
+import org.apache.poi.xwpf.usermodel.XWPFDocument
+import org.springframework.stereotype.Service
+
+interface RestWebDocumentService {
+
+    fun export2Word()
+}
+
+@Service
+open class RestWebDocumentServiceImpl : RestWebDocumentService {
+    override fun export2Word() {
+        val doc = XWPFDocument()
+
+        // 创建一个段落
+        val paragraph = doc.createParagraph()
+        paragraph.alignment = ParagraphAlignment.CENTER
+
+    }
+
+}
