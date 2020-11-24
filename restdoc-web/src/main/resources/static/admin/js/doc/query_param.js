b2fa8fe5b2f58ee7a8c93bd4ea05349ae1c614ed
maxuefeng <m17793873123@163.com>
2020-11-24 20:33:47 +0800

    增加query_param_string和response_header的动态操作

---
+++ restdoc-web/src/main/resources/static/admin/js/doc/query_param.js
@@ -0,0 +1,14 @@
+var one_query_param_string_line = "<div class='layui-form-item one-query-param-string-line'  ><div class='layui-inline'><div class='layui-input-inline'>" +
+    "<input type='text' name='queryParamField' placeholder='Key' autocomplete='off'" +
+    "                               class='layui-input'> </div> </div> <div class='layui-inline'> <label>=</label> </div>  <div class='layui-inline'>" +
+    "                    <div class='layui-input-inline'>" +
+    "                        <input type='text' name='queryParamValue' placeholder='Value' autocomplete='off'" +
+    "                               class='layui-input'>" +
+    "                    </div>" +
+    "                </div>" +
+    " <div class='layui-inline'>" +
+    "                    <button title='删除当前字段' type='button' class='layui-btn layui-btn-sm layui-btn-primary' id='deleteQueryParamBtn'" +
+    "                            style='margin-left: 10px'" +
+    "                            onclick='deleteQueryParamFieldLine(this)'>" +
+    "                        <i class='layui-icon layui-icon-subtraction' style='font-size: 30px; color: black;'></i></button>" +
+    "            </div>";
---
+++ restdoc-web/src/main/resources/static/admin/js/doc/response_header.js
@@ -0,0 +1,14 @@
+var one_response_header_line = "<div class='layui-form-item oone_response_header_line'  ><div class='layui-inline'><div class='layui-input-inline'>" +
+    "<input type='text' name='responseHeaderField' placeholder='Key' autocomplete='off'" +
+    "                               class='layui-input'> </div> </div> <div class='layui-inline'> <label>=</label> </div>  <div class='layui-inline'>" +
+    "                    <div class='layui-input-inline'>" +
+    "                        <input type='text' name='responseHeaderValue' placeholder='Value' autocomplete='off'" +
+    "                               class='layui-input'>" +
+    "                    </div>" +
+    "                </div>" +
+    " <div class='layui-inline'>" +
+    "                    <button title='删除当前字段' type='button' class='layui-btn layui-btn-sm layui-btn-primary' id='deleteResponseHeaderBtn'" +
+    "                            style='margin-left: 10px'" +
+    "                            onclick='deleteResponseHeaderFieldLine(this)'>" +
+    "                        <i class='layui-icon layui-icon-subtraction' style='font-size: 30px; color: black;'></i></button>" +
+    "            </div>";
