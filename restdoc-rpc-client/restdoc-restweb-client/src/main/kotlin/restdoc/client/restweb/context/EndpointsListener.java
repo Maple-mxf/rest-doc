d8268354b4bf4a178df7b368cf008c3ec55cba6a
maxuefeng <m17793873123@163.com>
2020-12-03 14:28:52 +0800

    完善门户访问

--- restdoc-rpc-client/restdoc-restweb-client/src/main/kotlin/restdoc/client/restweb/context/EndpointsListener.java
+++ restdoc-rpc-client/restdoc-restweb-client/src/main/kotlin/restdoc/client/restweb/context/EndpointsListener.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 import org.springframework.util.MimeType;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.condition.MediaTypeExpression;
